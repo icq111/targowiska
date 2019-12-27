f9de0b91a1b2f7c48129952d3b1184e6738a4588
Michal Spiewak <minicoderzeszow@gmail.com>
2019-12-27 21:52:36 +0100

    Remove bootstrap Add w3.css framework

--- targowiska/src/main/java/pl/minicode/targowiska/controller/WelcomeController.java
+++ targowiska/src/main/java/pl/minicode/targowiska/controller/WelcomeController.java
@@ -1,37 +1,14 @@
 package pl.minicode.targowiska.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
-	// inject via application.properties
-	@Value("${welcome.message}")
-	private String message;
-
-
-
 	@GetMapping("/")
 	public String showUsersListForm(Model model) {
 		
 		return "welcome"; // view
 	}
-	
-	@GetMapping("/admin-panel")
-	public String showAdminPanelForm(Model model) {
-		
-		return "admin_panel"; // view
-	}
-
-//	// /hello?name=kotlin
-//	@GetMapping("/hello")
-//	public String mainWithParam(@RequestParam(name = "name", required = false, defaultValue = "Simple user") String name,
-//			Model model) {
-//		model.addAttribute("users", userRepository.findAll());
-//		model.addAttribute("message", name);
-//
-//		return "welcome"; // view comment
-//	}
 }
